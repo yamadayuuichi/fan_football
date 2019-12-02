86a51a223f5a7acefc61a87a06fd3f4f3dc7dfe6
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-12-02 16:51:44 +0900

    topicsController resources

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,30 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destory
+
+  end
+
+  private
 end
