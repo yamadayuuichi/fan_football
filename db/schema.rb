43c1ba3455d685361d213dcdbe7faaa462df06a2
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-12-05 14:05:28 +0900

    current_user notGoTopPage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_043858) do
+ActiveRecord::Schema.define(version: 2019_12_05_045324) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_043858) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
