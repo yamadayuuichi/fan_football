8314884c6cd01e8a02180bfa1bd678f5e4018f2d
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-12-07 00:28:58 +0900

    move_to_index

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,6 @@
 class TopicsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @topics = Topic.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -32,6 +34,10 @@ class TopicsController < ApplicationController
     params.permit(:title, :text)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+  
   def id_params
     params.permit(:id)
   end
