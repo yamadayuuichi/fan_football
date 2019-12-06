ef4eb47e49457871b06192b2671cfbb1a48439d4
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-12-07 02:12:14 +0900

    comment base

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ApplicationRecord
   belongs_to :user
   has_many :images
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_161459) do
+ActiveRecord::Schema.define(version: 2019_12_06_170912) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id"
+    t.bigint "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -42,6 +52,8 @@ ActiveRecord::Schema.define(version: 2019_12_06_161459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "topics"
+  add_foreign_key "comments", "users"
   add_foreign_key "images", "topics"
   add_foreign_key "topics", "users"
 end
